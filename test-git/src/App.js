cd746e0a7ed4082de91309bf3fb1c9ba8ea47b0b
Chechu <ceciliasosa1986@gmail.com>
2023-11-01 13:47:25 +1300

    Agrego Header

--- test-git/src/App.js
+++ test-git/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Prueba 1
+      <header>
+        <h1>Este es el nuevo Header</h1>
+        <h2>Estamos probando git</h2>
+      </header>  
     </div>
   );
 }
